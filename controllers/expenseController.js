ce958b56dfae1c843c29dc91bbce24b929c63f9b
Gal <galsinai33@gmail.com>
2023-02-03 11:53:47 +0200

    reports commit

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -44,7 +44,7 @@ const addExpense = async (req, res) => {
 };
 
 // @desc   Get all current user's expenses for a specific month and a year
-// @route  /report
+// @route  /report/all-expenses
 // @access Private
 const getAllExpenses = async (req, res) => {
   const { month, year } = req.body;
@@ -78,7 +78,81 @@ const getAllExpenses = async (req, res) => {
   }
 };
 
+// @desc   Get current user's expenses for a specific month during the current year
+// @route  /report/month
+// @access Private
+const getMonthlyExpenses = async (req, res) => {
+  const { month } = req.body;
+  const currentUser = await User.find({ id: req.user.id });
+  const todaysDate = new Date();
+
+  try {
+    const docs = await Expense.find({
+      month: month,
+      year: todaysDate.getFullYear(),
+      user_id: currentUser[0].id,
+    });
+
+    if (docs.length === 0) {
+      res.status(404).json({
+        status: `This account have no expenses in this month (${month}).`,
+      });
+      throw new Error(
+        `This account have no expenses in this month (${month}).`
+      );
+    }
+
+    res.status(200).json({
+      status: "success",
+      requestedAt: req.requestTime,
+      results: docs.length,
+      data: {
+        data: docs,
+      },
+    });
+  } catch (error) {
+    res.status(400);
+    throw new Error("Could not get monthly expenses");
+  }
+};
+
+// @desc   Get all current user's expenses for a specific year
+// @route  /report/year
+// @access Private
+const getYearlyExpenses = async (req, res) => {
+  const { year } = req.body;
+  const currentUser = await User.find({ id: req.user.id });
+
+  try {
+    const docs = await Expense.find({
+      year: year,
+      user_id: currentUser[0].id,
+    });
+
+    if (docs.length === 0) {
+      res.status(404).json({
+        status: "There are no expenses in this account in that year.",
+      });
+      throw new Error("There are no expenses in this account in that year.");
+    }
+
+    res.status(200).json({
+      status: "success",
+      requestedAt: req.requestTime,
+      results: docs.length,
+      data: {
+        data: docs,
+      },
+    });
+  } catch (error) {
+    res.status(400);
+    throw new Error("Could not get that year's expenses");
+  }
+};
+
 module.exports = {
   addExpense,
   getAllExpenses,
+  getMonthlyExpenses,
+  getYearlyExpenses,
 };
--- routes/expenseRoute.js
+++ routes/expenseRoute.js
@@ -4,9 +4,13 @@ const { protect } = require("../middleware/authMiddleware");
 const {
   addExpense,
   getAllExpenses,
+  getMonthlyExpenses,
+  getYearlyExpenses,
 } = require("../controllers/expenseController");
 
 router.post("/addcost", protect, addExpense);
-router.get("/report", protect, getAllExpenses);
+router.get("/report/all-expenses", protect, getAllExpenses);
+router.get("/report/month", protect, getMonthlyExpenses);
+router.get("/report/year", protect, getYearlyExpenses);
 
 module.exports = router;
